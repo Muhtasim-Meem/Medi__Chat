55e25979be2d5d7db846bffc2adeedd5f213fa40
Muhtasim <muhtasimrahman810@gmail.com>
2025-06-06 10:56:59 +0600

    Project structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ="Generative AI Project",
+    version="0.0.1",
+    author="Md. Muhtasim Rahman",
+    author_email="muhtasimrahman810@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
